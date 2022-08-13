fca6781831fd7805c228202bcf1ebde27be86f7d
Sifatulla21 <sifat2109@gmail.com>
2022-08-13 17:00:21 +0600

    first commit

---
+++ src/pharmacymanagement/AbstractClass.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pharmacymanagement;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author USER
+ */
+abstract class AbstractClass extends Buttons{
+    public abstract void clearAll();
+}
